4bf5c0e55cd128b4d54ab1166a4ec5edf55d47ee
Yunminseon1 <dbsalstjssla@naver.com>
2026-09-14 18:54:21 +0900

    [level 0] Title: 분수의 덧셈, Time: 3.38 ms, Memory: 87.9 MB -BaekjoonHub

--- Java/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ Java/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,11 +1,14 @@
+// 1. 두 분모를 최소공배수로 맞춰준다.
+// 2. 두 분수의 합을 배열에 담는다.
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        
         int[] answer = {};
         int max = 0;
-        
+
         int numer = numer1*denom2 + numer2*denom1;
         int denom = denom1*denom2;
-        
+
         for (int i=1; i<=denom && i<=numer; i++){
             if(numer%i ==0 && denom%i==0){
                 max = i;
@@ -14,7 +17,7 @@ class Solution {
 
         numer = numer/max;
         denom = denom/max;
-        
+
         answer = new int[]{numer, denom};
 
         return answer;
